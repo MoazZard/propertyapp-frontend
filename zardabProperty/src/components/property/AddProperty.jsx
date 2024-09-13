f4bd27a6ef0dafa293d284cbd032040f722155a5
Moaz Zardab <moazzardab@gmail.com>
2024-09-13 23:52:52 +0100

    more functions

--- zardabProperty/src/components/property/AddProperty.jsx
+++ zardabProperty/src/components/property/AddProperty.jsx
@@ -12,8 +12,8 @@ const AddProperty = () => {
 
     // Image preview 
     const [imagePreview, setImagePreview] = useState("")
-    // Error messages managers
-    const [successMessage, setSuccesMessage] = useState("")
+    // Error/Success messages managers
+    const [successMessage, setSuccessMessage] = useState("")
     const [errorMessage, setErrorMessage] = useState("")
 
 
@@ -44,24 +44,80 @@ const AddProperty = () => {
             ...newProperty,
             photo: selectedImage
         })
-        setImagePreview(URL.createObjectURL(selectedImage))
+        setImagePreview(URL.createObjectURL(selectedImage)) //imagePreview becomes the image URL
     }
 
     // handles submit event and calls addProperty function from ApiFunctions
-    const handleSubmit = async(e) => { 
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        try{
+        try {
             const success = await addProperty(newProperty.photo, newProperty.propertyType, newProperty.propertyPrice)
-        }catch(error){
+            if (success !== undefined) {
+                setSuccessMessage("A new property has been added to the database.")
+                setNewProperty({
+                    photo: null,
+                    propertyType: "",
+                    propertyPrice: ""
+                })
+                setImagePreview("")
+                setErrorMessage("")
+            }
+            else {
+                setErrorMessage("Error adding property to database")
+            }
+        } catch (error) {
             setErrorMessage(error.message)
         }
     }
 
-
+    // UI
     return (
-        <div>
+        <>
+            <section className="container, mt-5 mb-5">
+                <div className="row justify-content-centre">
+                    <div className="col-md-8 col-lg-6">
+                        <h2 className="mt-5 mb-2">Add a New Property</h2>
 
-        </div>
+
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label htmlFor="propertyType" className="form-label">
+                                    Property Type
+                                </label>
+                                <div></div> 
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="propertyPrice" className="form-label">
+                                    Property Price
+                                </label>
+                                <input className="form-control" required id="propertyPrice" type="number"
+                                    name="propertyPrice" value={newProperty.propertyPrice} onChange={handlePropertyInputChange} />
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="photo" className="form-label">
+                                    Property Photo
+                                </label>
+                                <input id="photo" name="photo" type="file"
+                                    className="form-control" onChange={handleImageChange} />
+
+                                {/* if imagepreview isnt null, present the image */}
+                                {imagePreview && (<img src={imagePreview} alt="Property Preview Photo" //imageprev changed in handleImageChange
+                                    style={{ maxWidth: "400px", maxHeight: "400px" }}
+                                    className="mb-3" />)}
+                            </div>
+
+                            <div className="d-grid d-md-flex mt-2">
+                                <button className="btn btn-outline-primary ml-5">Save Property</button>
+                            </div>
+                        </form>
+
+
+                    </div>
+                </div>
+            </section>
+        </>
     );
 }
 
